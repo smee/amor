6abe8b3782daf7f3bca0b97c30bde7d1450423ba
sd <sd@206cfb95-553d-664c-9e80-73077bd90037>
2009-08-18 10:06:09 +0000

    start of stupid "dump everything into a revision directory" storage implementation
    git-svn-id: https://svn.model-repository.de/trunk@51 206cfb95-553d-664c-9e80-73077bd90037

--- org.infai.amor.backend.impl/src/org/infai/amor/backend/internal/storage/BlobStorage.java
+++ org.infai.amor.backend.impl/src/org/infai/amor/backend/internal/storage/BlobStorage.java
@@ -9,13 +9,20 @@
  *******************************************************************************/
 package org.infai.amor.backend.internal.storage;
 
+import java.io.File;
+import java.net.URISyntaxException;
+
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xml.type.internal.DataValue.URI.MalformedURIException;
 import org.infai.amor.backend.ChangedModel;
 import org.infai.amor.backend.CommitTransaction;
 import org.infai.amor.backend.Model;
 import org.infai.amor.backend.Response;
+import org.infai.amor.backend.exception.TransactionException;
+import org.infai.amor.backend.internal.responses.CheckinResponse;
 import org.infai.amor.backend.storage.Storage;
 
 /**
@@ -26,6 +33,13 @@ import org.infai.amor.backend.storage.Storage;
  */
 public class BlobStorage implements Storage {
 
+    private final File storageDir;
+    private ResourceSetImpl resourceSet;
+
+    public BlobStorage(final File storageDir) {
+        this.storageDir = storageDir;
+    }
+
     /*
      * (non-Javadoc)
      * 
@@ -34,8 +48,10 @@ public class BlobStorage implements Storage {
      */
     @Override
     public Response checkin(final ChangedModel model, final CommitTransaction tr) {
-        // TODO Auto-generated method stub
-        return null;
+        final URI fileURI = createUriFor(model.getPath(), tr);
+
+        // TODO create real repository uri usable as external reference
+        return new CheckinResponse("Checked in model [" + model.getPath().lastSegment() + "]", null);
     }
 
     /*
@@ -58,6 +74,80 @@ public class BlobStorage implements Storage {
     public Model checkout(final URI uri) throws MalformedURIException {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.infai.amor.backend.exception.TransactionListener#commit(org.infai.amor.backend.CommitTransaction)
+     */
+    @Override
+    public void commit(final CommitTransaction tr) throws TransactionException {
+        // nothing to do
+        resourceSet = null;
+    }
+
+    /**
+     * @param modelPath
+     * @param tr
+     * @return
+     */
+    protected URI createUriFor(final IPath modelPath, final CommitTransaction tr) {
+        String dirName = tr.getBranch().getName() + File.separatorChar + Long.toString(tr.getRevisionId());
+        // if there is a model path, use its relative directory part
+        if (modelPath != null && !modelPath.isAbsolute()) {
+            final int numSegments = modelPath.segmentCount();
+            for (int i = 0; i < numSegments - 1; i++) {
+                dirName = dirName + File.separatorChar + modelPath.segment(i);
+            }
+        }
+        // create new directory for this revision
+        final File dir = new File(storageDir, dirName);
+        dir.mkdirs();
+        // create uri for this new directory
+        final URI fileUri = URI.createURI(dir.toURI().toString());
+        return fileUri;
+    }
+
+    /**
+     * @param dir
+     */
+    private void deleteRecurively(final File dir) {
+        if (dir.isDirectory()) {
+            for (final File file : dir.listFiles()) {
+                deleteRecurively(file);
+            }
+        }
+        dir.delete();
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.infai.amor.backend.exception.TransactionListener#rollback(org.infai.amor.backend.CommitTransaction)
+     */
+    @Override
+    public void rollback(final CommitTransaction tr) {
+        final URI fileURI = createUriFor(null, tr);
+        // delete the directory of this revision
+        try {
+            final File dir = new File(new java.net.URI(fileURI.toString()));
+            deleteRecurively(dir);
+        } catch (final URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.infai.amor.backend.exception.TransactionListener#startTransaction(org.infai.amor.backend.CommitTransaction)
+     */
+    @Override
+    public void startTransaction(final CommitTransaction tr) {
+        resourceSet = new ResourceSetImpl();
     }
 
     /*
