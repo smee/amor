3448a60fbcf6caa8cb3076a1ef60dba2f34b0cfe
sh <sh@206cfb95-553d-664c-9e80-73077bd90037>
2009-08-22 19:44:36 +0000

    git-svn-id: https://svn.model-repository.de/trunk@77 206cfb95-553d-664c-9e80-73077bd90037

--- Test/de.model-repository.test/src/de/modelrepository/test/ModelComparator.java
+++ Test/de.model-repository.test/src/de/modelrepository/test/ModelComparator.java
@@ -1,4 +1,4 @@
-package de.model.repository.test;
+package de.modelrepository.test;
 
 import java.io.File;
 import java.io.IOException;
--- Test/de.model-repository.test/src/de/modelrepository/test/ProxyException.java
+++ Test/de.model-repository.test/src/de/modelrepository/test/ProxyException.java
@@ -1,4 +1,4 @@
-package de.model.repository.test;
+package de.modelrepository.test;
 
 import java.util.Iterator;
 import java.util.Vector;
