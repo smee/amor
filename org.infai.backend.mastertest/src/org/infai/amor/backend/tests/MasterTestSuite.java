ca18de2f446ea363d6dccb0ddf58399dea2b8ffb
sd <sd@206cfb95-553d-664c-9e80-73077bd90037>
2010-02-16 13:53:24 +0000

    aggregation of test suites. Allows to run them all in one suite.
    git-svn-id: https://svn.model-repository.de/trunk@233 206cfb95-553d-664c-9e80-73077bd90037

---
+++ org.infai.backend.mastertest/src/org/infai/amor/backend/tests/MasterTestSuite.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2010 InfAI.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *******************************************************************************/
+package org.infai.amor.backend.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author sdienst Run with the following parameters: -ea -Xmx1000M -Djava.util.logging.config.file=logging.properties
+ */
+@RunWith(Suite.class)
+@SuiteClasses( {
+    org.infai.amor.backend.neoblobstorage.test.AllTests.class,
+    org.infai.amor.backend.impl.AllTests.class,
+    org.infai.amor.backend.filestorage.test.AllTests.class,
+})
+public class MasterTestSuite {
+
+}
