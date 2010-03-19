e78a15722b3e0c73157bfcaccd6b41144f5b5bd7
jhm <jhm@206cfb95-553d-664c-9e80-73077bd90037>
2010-03-19 16:03:29 +0000

    git-svn-id: https://svn.model-repository.de/trunk@274 206cfb95-553d-664c-9e80-73077bd90037

---
+++ org.infai.amor.backend.resources/src/org/amor/infai/backend/resources/AmorResourceFactoryImpl.java
@@ -0,0 +1,12 @@
+package org.amor.infai.backend.resources;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
+
+public class AmorResourceFactoryImpl extends ResourceFactoryImpl{
+
+	public Resource createResource(URI uri){
+		return new AmorResourceImpl(uri);
+	}
+}
