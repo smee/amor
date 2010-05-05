7295169c22270c93c76de4895310e7fc519b9674
sd <sd@206cfb95-553d-664c-9e80-73077bd90037>
2010-05-05 13:56:10 +0000

    [docu] added more docu for clients
    git-svn-id: https://svn.model-repository.de/trunk@289 206cfb95-553d-664c-9e80-73077bd90037

--- org.infai.amor.backend.api/src/org/infai/amor/backend/api/SimpleRepository.java
+++ org.infai.amor.backend.api/src/org/infai/amor/backend/api/SimpleRepository.java
@@ -23,31 +23,45 @@ public interface SimpleRepository {
     final int ADDED = 0;
     final int CHANGED = 1;
     final int DELETED = 2;
+
     /**
-     * Checkin a new metamodel that is not known to this repository instance yet. Returns a list of namespaces of all packages
-     * that the give ecore depends on and which are yet unknown to this repository. All of these dependecies need to be checked in prior to commiting, else
-     * this model won't be persisted.
+     * Checkin a new (meta-)model that is not known to this repository instance yet. <br/>
+     * <br/>
+     * If it's a metamodel: Returns a list of namespaces of all packages that the give ecore depends on and which are yet unknown
+     * to this repository. All of these dependecies need to be checked in prior to commiting, else this model won't be persisted. <br/>
+     * <br/>
+     * If it's an instance model: Returns a mixed list: Namespaces of metamodels that this model depends on as well as relative
+     * paths to other instance models that this model references.
      * 
-     * @param ecorexmi xmi representation of the ecore metamodell to checkin
+     * @param ecorexmi
+     *            xmi representation of the ecore metamodell to checkin
      * @param relativePath
      * @return
      */
     List<String> checkin(String ecoreXmi, String relativePath, long transactionId);
 
     /**
+     * Checkin a serialized epatch that should be applied to the most recent model at the given relative path of the current
+     * transaction's branch.
+     * 
      * @param epatch
+     *            serialized epatch
      * @param relativePath
+     *            relative path of the model
      * @param transactionId
+     *            current transaction
      * @throws RuntimeException
      *             placeholder, TODO
      */
     void checkinPatch(String epatch, String relativePath, long transactionId) throws RuntimeException;
 
     /**
+     * Checkout serialized model from branch/revision.
+     * 
      * @param branch
      * @param revisionId
      * @param relativePath
-     * @return
+     * @return serialized xmi representation of the model
      * @throws IOException
      */
     String checkout(String branch, long revisionId, String relativePath) throws IOException;
@@ -69,7 +83,7 @@ public interface SimpleRepository {
      *            name of the new branch
      * @param oldbranchname
      *            null or name of the parent branch
-     * @param startRevisionIdw
+     * @param startRevisionId
      *            -1 or a valid revisionid that resembles the {@link Branch#getHeadRevision()} of the new branch
      * @throws TODO
      *             which exceptions exactly?
@@ -94,15 +108,28 @@ public interface SimpleRepository {
      */
     String[] getBranches();
 
+    /**
+     * Get relative paths of models that were touched in the given revision.
+     * 
+     * @param branchname
+     * @param revisionId
+     * @param changeType
+     *            type of change, could be {@link #ADDED}, {@link #CHANGED} or {@link #DELETED}
+     * @return list of relative paths
+     */
     List<String> getTouchedModelPaths(String branchname,long revisionId,int changeType);
 
     /**
+     * Abort checkin transactions, removes all changes applied since {@link #startTransaction(String)}.
+     * 
      * @param transactionId
      * @throws Exception
      */
     void rollbackTransaction(long transactionId) throws Exception;
 
     /**
+     * Start new write transaction.
+     * 
      * @see Repository#startCommitTransaction(Branch)
      * @param branchname
      * @return
