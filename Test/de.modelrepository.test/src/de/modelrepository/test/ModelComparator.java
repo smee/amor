4e3747aa2b428b68b0b3816f053dd5929a20edd6
sd <sd@206cfb95-553d-664c-9e80-73077bd90037>
2010-06-07 14:59:01 +0000

    [fix] workaround for bug in Epatch right now, see https://bugs.eclipse.org/bugs/show_bug.cgi?id=315738
    git-svn-id: https://svn.model-repository.de/trunk@302 206cfb95-553d-664c-9e80-73077bd90037

--- Test/de.modelrepository.test/src/de/modelrepository/test/ModelComparator.java
+++ Test/de.modelrepository.test/src/de/modelrepository/test/ModelComparator.java
@@ -17,59 +17,60 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
 public class ModelComparator {
-	private MatchModel match;
-	private DiffModel diff;
-	/**
-	 * Compares two models, each given as an {@link EObject}.
-	 * @param model1 first model
-	 * @param model2 second model
-	 * @return a {@link DiffModel} which contains all differences between both models.
-	 */
-	public DiffModel compare(EObject model1, EObject model2) throws InterruptedException {
-		match = MatchService.doMatch(model1, model2, Collections.<String, Object> emptyMap());
-		diff = DiffService.doDiff(match, false);
-		return diff;
-	}
-	
-	/**
-	 * Method compares two emf model files.
-	 * @param model1 first model
-	 * @param model2 second model
-	 * @return a {@link DiffModel} which contains all differences between both models.
-	 */
-	public DiffModel compare(File model1, File model2) throws InterruptedException, IOException {
-		if(model1 != null && model2 != null) {
-			if(model1.exists() && model1.exists()) {
-				ResourceSet rs = new ResourceSetImpl();
-				EObject m1 = ModelUtils.load(model1, rs);
-				EObject m2 = ModelUtils.load(model2, rs);
-				
-				return compare(m1, m2);
-			}
-		}
-		return null;
-	}
-	
-	/**
-	 * This method creates an Epatch from the previously parsed model files.
-	 * @return the created Epatch.
-	 */
-	public Epatch getEpatch() {
-		if(match == null || diff == null) {
-			return null;
-		}
-		return DiffEpatchService.createEpatch(match, diff, "patch");
-	}
-	
-	/**
-	 * This method compares two resources instead of EObjects.
-	 * @param r1 the first resource (source)
-	 * @param r2 the second resource (target)
-	 * @return the difference model.
-	 */
-	public DiffModel compare(Resource r1, Resource r2) throws InterruptedException {
-		match = MatchService.doResourceMatch(r1, r2, null);
-		diff = DiffService.doDiff(match, false);
-		return diff;
-	}
+    private MatchModel match;
+    private DiffModel diff;
+    /**
+     * Compares two models, each given as an {@link EObject}.
+     * @param model1 first model
+     * @param model2 second model
+     * @return a {@link DiffModel} which contains all differences between both models.
+     */
+    public DiffModel compare(EObject model1, EObject model2) throws InterruptedException {
+        match = MatchService.doMatch(model1, model2, Collections.<String, Object> emptyMap());
+        // diff = DiffService.doDiff(match, false);
+        diff = new WorkaroundDiffEngine().doDiff(match, false);
+        return diff;
+    }
+
+    /**
+     * Method compares two emf model files.
+     * @param model1 first model
+     * @param model2 second model
+     * @return a {@link DiffModel} which contains all differences between both models.
+     */
+    public DiffModel compare(File model1, File model2) throws InterruptedException, IOException {
+        if(model1 != null && model2 != null) {
+            if(model1.exists() && model1.exists()) {
+                ResourceSet rs = new ResourceSetImpl();
+                EObject m1 = ModelUtils.load(model1, rs);
+                EObject m2 = ModelUtils.load(model2, rs);
+
+                return compare(m1, m2);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * This method creates an Epatch from the previously parsed model files.
+     * @return the created Epatch.
+     */
+    public Epatch getEpatch() {
+        if(match == null || diff == null) {
+            return null;
+        }
+        return DiffEpatchService.createEpatch(match, diff, "patch");
+    }
+
+    /**
+     * This method compares two resources instead of EObjects.
+     * @param r1 the first resource (source)
+     * @param r2 the second resource (target)
+     * @return the difference model.
+     */
+    public DiffModel compare(Resource r1, Resource r2) throws InterruptedException {
+        match = MatchService.doResourceMatch(r1, r2, null);
+        diff = DiffService.doDiff(match, false);
+        return diff;
+    }
 }
