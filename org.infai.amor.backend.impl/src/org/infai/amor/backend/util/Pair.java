d825981d29d54ad30156f7de234e8bb9384ed4f9
sd <sd@206cfb95-553d-664c-9e80-73077bd90037>
2010-07-05 11:36:46 +0000

    [internal] added tostring
    git-svn-id: https://svn.model-repository.de/trunk@320 206cfb95-553d-664c-9e80-73077bd90037

--- org.infai.amor.backend.impl/src/org/infai/amor/backend/util/Pair.java
+++ org.infai.amor.backend.impl/src/org/infai/amor/backend/util/Pair.java
@@ -8,11 +8,21 @@ package org.infai.amor.backend.util;
  *
  */
 public class Pair<S,T> {
-	public final S first;
-	public final T second;
-	
-	public Pair(S f, T s){
-		this.first = f;
-		this.second = s;
-	}
+    public final S first;
+    public final T second;
+
+    public Pair(S f, T s){
+        this.first = f;
+        this.second = s;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return String.format("<%s:%s>", first.toString(), second.toString());
+    }
 }
